f105ccaba37477483d89582f9ec8284e4a9fa69b
Petrova Daria <DaArtPetrova@mail.ru>
2020-01-07 18:28:05 +0500

    Updates

---
+++ precache-manifest.680acd03d943d0985ca6a92ce97dd518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc2888c4aa989c5222ba3099ffd9e69",
+    "url": "/road-safety-orderliness-method/index.html"
+  },
+  {
+    "revision": "9d315959ac23140de335",
+    "url": "/road-safety-orderliness-method/static/css/main.670489b0.chunk.css"
+  },
+  {
+    "revision": "aca49d70784d4bfe3adc",
+    "url": "/road-safety-orderliness-method/static/js/2.4168abde.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/road-safety-orderliness-method/static/js/2.4168abde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d315959ac23140de335",
+    "url": "/road-safety-orderliness-method/static/js/main.dd4cfa40.chunk.js"
+  },
+  {
+    "revision": "a509767d364ce2426b25",
+    "url": "/road-safety-orderliness-method/static/js/runtime-main.d3ca08c5.js"
+  }
+]);
